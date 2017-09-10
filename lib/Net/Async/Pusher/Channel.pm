c9053a6a61bcdfd68dc71ab3e89e255454622261
Tom Molesworth <TEAM@cpan.org>
2017-09-11 04:29:05 +0800

    Support ->source on channels

--- lib/Net/Async/Pusher/Channel.pm
+++ lib/Net/Async/Pusher/Channel.pm
@@ -3,9 +3,11 @@ package Net::Async::Pusher::Channel;
 use strict;
 use warnings;
 
+use parent qw(IO::Async::Notifier);
+
 =head1 NAME
 
-Net::Async::Pusher::Connection - represents one L<Net::Async::Pusher> server connection
+Net::Async::Pusher::Channel - represents a channel on a L<Net::Async::Pusher::Connection>.
 
 =head1 DESCRIPTION
 
@@ -13,19 +15,25 @@ Provides basic integration with the L<https://pusher.com|Pusher> API.
 
 =cut
 
+use Syntax::Keyword::Try;
 use Mixin::Event::Dispatch::Bus;
 use JSON::MaybeXS;
+use Ryu::Async;
+
 use Log::Any qw($log);
 
-sub new {
-	my ($class) = shift;
-	bless { @_ }, $class
+sub configure {
+    my ($self, %args) = @_;
+    for (qw(name)) {
+        $self->{$_} = delete $args{$_} if exists $args{$_};
+    }
+    return $self->SUPER::configure(%args);
 }
-sub loop { shift->{loop} }
 
 sub bus { shift->{bus} //= Mixin::Event::Dispatch::Bus->new }
-
 sub json { shift->{json} //= JSON::MaybeXS->new( allow_nonref => 1) }
+
+sub ryu { shift->{ryu} }
 
 sub subscribe {
 	my ($self) = shift;
@@ -37,9 +45,11 @@ sub subscribe {
 	}
 	Future->done(sub {
 		while(my ($k, $v) = splice @sub, 0, 2) {
-			eval {
+			try {
 				$self->bus->unsubscribe_from_event($k => $v);
-			};
+			} catch {
+                $log->errorf('Failed to unsubscribe from %s - %s', $k, $@);
+            }
 		}
 		Future->done;
 	})
@@ -50,17 +60,42 @@ sub incoming_message {
 	if($info->{event} eq 'pusher_internal:subscription_succeeded') {
 		return $self->subscribed->done
 	} else {
-		eval {
+		try {
 			my $data = $self->json->decode($info->{data});
-			$self->bus->invoke_event(
-				"event::" . $info->{event} => $data
-			);
+            if(my $bus = $self->{bus}) {
+                $self->bus->invoke_event(
+                    "event::" . $info->{event} => $data
+                );
+            }
+            if(my $src = $self->{source}) {
+                $src->emit($info);
+            }
 			1
-		} or do $log->errorf("Exception [%s] on %s", $@, $info->{data})
+		} catch {
+            $log->errorf("Exception [%s] on %s", $@, $info->{data});
+        }
 	}
 }
 
 sub subscribed { $_[0]->{subscribed} //= $_[0]->loop->new_future }
+
+=head2 source
+
+Returns a L<Ryu::Source> which will emit an item for each event on the channel.
+
+=cut
+
+sub source {
+    my ($self) = @_;
+    $self->{source} //= $self->ryu->source
+}
+
+sub _add_to_loop {
+    my ($self) = @_;
+    $self->add_child(
+        $self->{ryu} = Ryu::Async->new
+    )
+}
 
 1;
 
@@ -72,4 +107,4 @@ Tom Molesworth <TEAM@cpan.org>
 
 =head1 LICENSE
 
-Copyright Tom Molesworth 2015-2016. Licensed under the same terms as Perl itself.
+Copyright Tom Molesworth 2015-2017. Licensed under the same terms as Perl itself.
