edf80a7c372b3195ed755d21e750b14b459dcafc
Tom Molesworth <TEAM@cpan.org>
2021-01-29 07:51:08 +0800

    Update copyright dates

--- lib/Net/Async/Pusher.pm
+++ lib/Net/Async/Pusher.pm
@@ -56,4 +56,4 @@ Tom Molesworth <TEAM@cpan.org>
 
 =head1 LICENSE
 
-Copyright Tom Molesworth 2015-2017. Licensed under the same terms as Perl itself.
+Copyright Tom Molesworth 2015-2021. Licensed under the same terms as Perl itself.
